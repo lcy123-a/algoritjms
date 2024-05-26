97d65208269a48b4f37ff269b3dfb159ee241dba
chen <1123527836@qq.com>
2024-05-26 09:52:32 +0800

    红黑树

--- head/RBTREE.h
+++ head/RBTREE.h
@@ -495,3 +495,25 @@ private:
 };
 
 
+// int main(int argc, char *argv[])
+// {
+//     RBTree<int> rb;
+
+//     int arr[] = {10, 7, 8, 15, 5, 6, 11, 13, 12};
+//     int n = sizeof(arr) / sizeof(int);
+//     for (int i = 0; i < n; i++)
+//     {
+//         rb.Insert(arr[i]);
+//     }
+
+//     rb.InOrder();
+//     cout << endl;
+//     rb.InOrderPrint();
+//     cout << endl;
+//     rb.Remove(10);
+//     rb.InOrder();
+//     cout << endl;
+//     rb.Remove(21);
+//     system("pause");
+//     return 0;
+// }
